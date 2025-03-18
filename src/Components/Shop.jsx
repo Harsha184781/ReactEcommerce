e7fee98f8bb8d9b6f2ec17a51504305c226c53ba
Harsha184781 <sriharshabattu@gmail.com>
2025-03-18 11:35:38 +0530

    shop page changes

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -1,13 +1,43 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
 import table from "../assets/Images/table.jpg"
-import { FaSearch } from 'react-icons/fa'
+import { FaSearch, FaStar } from 'react-icons/fa'
+import { products } from '../products'
+import { Link } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import { CiCirclePlus } from 'react-icons/ci'
 
 
-const Shop = () => {
+
+const Shop = () => { 
+  let [search,Setsearch] = useState("")
+  let [data,Setdata] = useState([])
+  let [option,Setoption] = useState("")
+ const notify = () => toast.success("Product has been added to Cart");
+
+useEffect(()=>{
+     if(option===""){
+      Setdata(products)
+      console.log(data);
+     }
+     else{
+      Setdata(products.filter((ele)=>ele.category===option))
+      console.log(data);
+     }
+},[option])
 
 
-  const {} = useState("")
+
+  const handle = (option)=>{
+           Setoption(option)
+  }
+
+  const searchfun=(event)=>{
+      const {value} = event.target
+      Setsearch(value)
+      Setdata( products.filter((ele)=>ele.productName.toLowerCase().includes(value.toLowerCase())))
+  }
+
     return (
     <>
         <Navbar/>
@@ -35,24 +65,56 @@ const Shop = () => {
 
 
                           <div className="dropdown col-3">
-  <button style={{backgroundColor:"darkblue",marginLeft:"45px"}} className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+  <button style={{backgroundColor:"darkblue",marginLeft:"25px"}} className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
     Filter By Catergory<span style={{paddingLeft:"25px"}}>|</span>
   </button>
   <ul className="dropdown-menu">
-    <li><button className="dropdown-item" type="button">Sofa</button></li>
-    <li><button className="dropdown-item" type="button">Watch</button></li>
-    <li><button className="dropdown-item" type="button">Chair</button></li>
-    <li><button className="dropdown-item" type="button">Mobile</button></li>
-    <li><button className="dropdown-item" type="button">Wireless</button></li>
+    <li><button className="dropdown-item" type="button" onClick={()=>{handle("sofa")}}>Sofa</button></li>
+    <li><button className="dropdown-item" type="button" onClick={()=>{handle("watch")}}>Watch</button></li>
+    <li><button className="dropdown-item" type="button" onClick={()=>{handle("chair")}}>Chair</button></li>
+    <li><button className="dropdown-item" type="button" onClick={()=>{handle("mobile")}}>Mobile</button></li>
+    <li><button className="dropdown-item" type="button" onClick={()=>{handle("wireless")}}>Wireless</button></li>
   </ul>
 </div>
   
   <div className='col-9'>
-<input  style={{height:"40px",borderRadius:"20px",borderWidth:"0px",width:"46vw",marginLeft:"30vh",paddingLeft:"20px",backgroundColor:"#e1e4e8"}} placeholder='Search...'  type='text'/>
+<input name="search" onChange={(event)=>searchfun(event)} style={{height:"40px",borderRadius:"20px",borderWidth:"0px",width:"46vw",marginLeft:"30vh",paddingLeft:"20px",backgroundColor:"#e1e4e8"}} placeholder='Search...'  type='text'/>
 <FaSearch style={{position:"relative",right:"35px",bottom:"3px"}}/>
 </div>
-
 </div>  
+
+<div className='row' style={{marginLeft:"60px"}}>
+  {       
+          data.length>0 ?  data.map((ele)=>(
+<div key={ele.id} className="card col-lg-4  m-2 shadow-sm border-0" style={{width: "25vw", padding: "10px" }} >
+                  <Link to={`/productdetails/${ele.id}`} style={{textDecoration:"none",}}>
+                    <img style={{ width: "15vw", height: "30vh" ,marginLeft:"40px"}} src={ele.imgUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h3 style={{color:"black"}}>{ele.productName}</h3>
+                      <div>
+                        <div className="d-flex">
+                          {
+                          [1,2,3,4,5].map((ele) => (
+                              <p key={ele} className='ps-1' style={{ color: "gold", fontSize: "20px" }}><FaStar /></p>
+                            ))
+                          }
+                        </div>
+                        <div className='d-flex justify-content-between'>
+                          <h3 style={{textDecoration:"none",color:"black"}} className='p-2'>{`$ ${ele.price}`}</h3>
+                        </div>
+                      </div>
+                    </div>
+                  </Link>
+                  <button style={{ border: "none", backgroundColor: "white", position: "absolute", left: "300px" , bottom:"50px"}} onClick={notify}><CiCirclePlus style={{ fontSize: "50px" }} /></button>
+                  <ToastContainer />
+                </div>
+
+              )) : <h1 style={{textAlign:"center",marginTop:"100px"}}>Product Not Found !!</h1>
+}
+</div>
+
+
+
 
 </div>
 
